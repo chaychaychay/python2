e976f9ca0a886f39ecb587c2ef0d3aa6d7f18e9e
chaychaychay <66235474+chaychaychay@users.noreply.github.com>
2021-01-08 10:09:10 +0700

    Create loop.py

---
+++ loop.py
@@ -0,0 +1,13 @@
+import time
+import random
+
+while True:
+    cpu = str(random.randrange(50,99))
+    print("======================================================================================================================")
+    print("           CPU USAGE: " + cpu + "%")
+    print("           SIM STATUS: ACTIVE")
+    print("           ERROR: 0")
+    print("           OS: Saturn 0.89.152 build 112 ALPHA [NOT PERMITTED FOR USE OUTSIDE OF CONTROLLED LAB]")
+    print("           MASTER CODE: false;")
+    print("======================================================================================================================")
+    time.sleep(1)
